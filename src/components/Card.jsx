3c15199c8e64bda1eadc465340e5d9d9179ca760
Sri <srivprasanna@gmail.com>
2023-06-05 22:23:14 -0400

    Added in card functionality, fixed up a few bugs. Awaiting Surya's Appwrite database files.
    Co-authored-by: SujayGolla <SujayGolla@users.noreply.github.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,25 +8,18 @@ import Typography from '@mui/material/Typography';
 
 export default function MediaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="https://images.unsplash.com/photo-1606489129187-1eee19a0a103?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Name</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Victim Description + Contact</p>
+    </div>
+</div>
+
   );
 }
