dc85e1e2c3f7810d583006da19a83138fa531b75
picafe <tallapaneni.surya@gmail.com>
2023-06-06 07:52:12 -0400

    clean: adjusted config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://amberhacks.picafe.me",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   }), mdx(), sitemap(), react()]
